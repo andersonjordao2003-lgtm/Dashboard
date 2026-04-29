230482a0b917df70a87876d3059a0e62723ace50
andersonjordao2003-lgtm <anderson.jordao2003@gmail.com>
2026-04-29 08:16:51 -0300

    Create auth.js

---
+++ api/auth.js
@@ -0,0 +1,32 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(400).json({ error: "No code" });
+  }
+
+  const params = new URLSearchParams();
+  params.append("client_id", process.env.CLIENT_ID);
+  params.append("client_secret", process.env.CLIENT_SECRET);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", process.env.REDIRECT_URI);
+
+  const tokenRes = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params
+  });
+
+  const tokenData = await tokenRes.json();
+
+  const userRes = await fetch("https://discord.com/api/users/@me", {
+    headers: {
+      Authorization: `Bearer ${tokenData.access_token}`
+    }
+  });
+
+  const user = await userRes.json();
+
+  return res.json(user);
+}
