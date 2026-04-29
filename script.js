218b396b598c9cc07aa3103a67dca4af67fb31c7
andersonjordao2003-lgtm <anderson.jordao2003@gmail.com>
2026-04-29 07:06:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function login() {
-  window.location.href = "https://discord.com/oauth2/authorize?client_id=SEU_CLIENT_ID&response_type=code&redirect_uri=https%3A%2F%2Fdashboard-amber-six-97.vercel.app%2F&scope=identify";
+  window.location.href = "https://discord.com/oauth2/authorize?client_id=1473101255999226084&response_type=code&redirect_uri=https%3A%2F%2Fdashboard-amber-six-97.vercel.app%2F&scope=identify";
 }
